e7ff2fd67d4520160dadd1fe0207ac170e7c1b45
Olli Raitio <olli.raitio@helsinki.fi>
2022-04-27 13:11:36 +0300

    fix ApiClientImplementation import in field service spec

--- test/server/field-service-spec.ts
+++ test/server/field-service-spec.ts
@@ -1,5 +1,5 @@
 import ApiClient from "laji-form/lib/ApiClient";
-import ApiClientImplementation from "../../src/server/view/ApiClientImplementation";
+import ApiClientImplementation from "../../src/server/api-client-implementation";
 import config from "../../config.json";
 import FieldService from "../../src/server/services/field-service";
 import MetadataService from "../../src/services/metadata-service";
