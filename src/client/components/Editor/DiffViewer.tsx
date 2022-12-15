45835b3cfcf10c1a0b475d1f2691620cb4bd700b
Olli Raitio <olli.raitio@helsinki.fi>
2022-12-15 19:49:49 +0200

    mv diff viewer to own file

---
+++ src/client/components/Editor/DiffViewer.tsx
@@ -0,0 +1,108 @@
+import * as React from "react";
+import { Master } from "../../../model";
+import { Context } from "../Context";
+import { makeCancellable, nmspc } from "../../utils";
+import diff, { Diff, DiffDeleted, DiffEdit, DiffNew } from "deep-diff";
+import memoize from "memoizee";
+
+export type DiffViewerProps = {
+	master: Master;
+}
+
+const DiffViewerModal = React.memo(function DiffViewerModal({master}: DiffViewerProps) {
+	const {formService} = React.useContext(Context);
+	const [remoteMaster, setRemoteMaster] = React.useState<Master | undefined>(undefined);
+	React.useEffect(() => {
+		if (!master.id) {
+			return;
+		}
+		const promise = makeCancellable(formService.getMaster(master.id).then(setRemoteMaster));
+		return promise.cancel;
+	}, [formService, master.id]);
+	return (
+		<DiffsViewer diffs={getDiff(remoteMaster, master)} />
+	);
+});
+
+export default DiffViewerModal;
+
+
+type NonArrayDiff = DiffNew<unknown> | DiffEdit<unknown> | DiffDeleted<unknown>;
+
+const getDiff = memoize((obj1: unknown, obj2: unknown) => {
+	const flattenArrays = (diffs: Diff<unknown>[]): NonArrayDiff[] => {
+		return diffs.reduce((_diffs, d) => {
+			if (d.kind === "A") {
+				_diffs.push({...d.item, path: [...(d.path || []), d.index]} as NonArrayDiff);
+			} else {
+				_diffs.push(d);
+			}
+			return _diffs;
+		}, [] as NonArrayDiff[]);
+	};
+	const diffs =	diff(obj1, obj2);
+	return diffs ? flattenArrays(diffs) : [];
+});
+
+const DiffPath = ({path}: Diff<unknown>) => <span>{path?.join(".")}</span>;
+
+const DiffKindMapper = (diff: Diff<unknown>) => {
+	switch (diff.kind) {
+	case "N":
+		return <DiffNewViewer {...diff} />;
+	case "D":
+		return <DiffDeletedViewer {...diff} />;
+	case "E":
+		return <DiffEditViewer {...diff} />;
+	default:
+		 return null;
+	}
+};
+
+const DiffNewViewer = (diff: DiffNew<unknown>) => {
+	return <span>{JSON.stringify(diff.rhs)}</span>;
+};
+
+const DiffDeletedViewer = (diff: DiffDeleted<unknown>) => {
+	return <span>{JSON.stringify(diff.lhs)}</span>;
+};
+
+const DiffEditViewer = (diff: DiffEdit<unknown>) => {
+	return <span>{`${JSON.stringify(diff.lhs)} ➞ ${JSON.stringify(diff.rhs)}`}</span>;
+};
+
+const diffNmspc = nmspc("diff");
+
+const mapDiffClassName = (kind: Diff<unknown>["kind"]) => {
+	switch (kind) {
+	case "N":
+		return diffNmspc("new");
+	case "D":
+		return diffNmspc("delete");
+	case "E":
+		return diffNmspc("edit");
+	default:
+		 return "";
+	}
+};
+
+const DiffViewerRow = (diff: Diff<unknown>) => (
+	<tr className={mapDiffClassName(diff.kind)}>
+		<th><DiffPath {...diff} /></th>
+		<td>
+			<DiffKindMapper {...diff} />
+		</td>
+	</tr>
+);
+
+const DiffsViewer = ({diffs}: {diffs: Diff<unknown>[]}) => {
+	const {theme} = React.useContext(Context);
+	const {Table} = theme;
+	return (
+		<Table bordered condensed>
+			<tbody>
+				{diffs.map(d => <DiffViewerRow key={d.path?.join() + d.kind} {...d}/>)}
+			</tbody>
+		</Table>
+	);
+};
--- src/client/components/Editor/Editor.tsx
+++ src/client/components/Editor/Editor.tsx
@@ -4,8 +4,9 @@ import memoize from "memoizee";
 import { 
 	DraggableHeight, DraggableWidth, Clickable, Button, Stylable, Classable, Spinner, FormJSONEditor, HasChildren
 } from "../components";
-import { classNames, nmspc, gnmspc, fieldPointerToSchemaPointer, fieldPointerToUiSchemaPointer, scrollIntoViewIfNeeded,
-	makeCancellable } from "../../utils";
+import {
+	classNames, nmspc, gnmspc, fieldPointerToSchemaPointer, fieldPointerToUiSchemaPointer, scrollIntoViewIfNeeded
+} from "../../utils";
 import { getPropertyContextName, parseJSONPointer, unprefixProp } from "../../../utils";
 import { ChangeEvent, TranslationsAddEvent, TranslationsChangeEvent, TranslationsDeleteEvent, UiSchemaChangeEvent,
 	FieldDeleteEvent, FieldAddEvent, FieldUpdateEvent, MaybeError, isValid } from "../Builder";
@@ -16,7 +17,7 @@ import OptionsEditor from "./OptionsEditor";
 import { Lang, Master, SchemaFormat, Field as FieldOptions } from "../../../model";
 import LajiForm from "laji-form/lib/components/LajiForm";
 import { findNearestParentSchemaElem, translate as translateKey } from "laji-form/lib/utils";
-import diff, { Diff, DiffDeleted, DiffEdit, DiffNew } from "deep-diff";
+import DiffViewer, { DiffViewerProps } from "./DiffViewer";
 
 export type FieldEditorChangeEvent =
 	TranslationsAddEvent
@@ -857,109 +858,17 @@ const FormJSONEditorModal = React.memo(function FormJSONEditorModal(
 	);
 });
 
-type DiffViewerProps = {
-	master: Master;
-	onHide: () => void;
-}
-
-type NonArrayDiff = DiffNew<unknown> | DiffEdit<unknown> | DiffDeleted<unknown>;
-
-const getDiff = memoize((obj1: unknown, obj2: unknown) => {
-	const flattenArrays = (diffs: Diff<unknown>[]): NonArrayDiff[] => {
-		return diffs.reduce((_diffs, d) => {
-			if (d.kind === "A") {
-				_diffs.push({...d.item, path: [...(d.path || []), d.index]} as NonArrayDiff);
-			} else {
-				_diffs.push(d);
-			}
-			return _diffs;
-		}, [] as NonArrayDiff[]);
-	};
-	const diffs =	diff(obj1, obj2);
-	return diffs ? flattenArrays(diffs) : [];
-});
-
-const DiffViewerModal = React.memo(function DiffViewerModal({master, onHide}: DiffViewerProps) {
-	const {formService} = React.useContext(Context);
-	const [remoteMaster, setRemoteMaster] = React.useState<Master | undefined>(undefined);
-	React.useEffect(() => {
-		if (!master.id) {
-			return;
-		}
-		const promise = makeCancellable(formService.getMaster(master.id).then(setRemoteMaster));
-		return promise.cancel;
-	}, [formService, master.id]);
-	return (
-		<GenericModal onHide={onHide}>
-			<DiffsViewer diffs={getDiff(remoteMaster, master)} />
-		</GenericModal>
-	);
-});
-
-const DiffPath = ({path}: Diff<unknown>) => <span>{path?.join(".")}</span>;
-
-const DiffKindMapper = (diff: Diff<unknown>) => {
-	switch (diff.kind) {
-	case "N":
-		return <DiffNewViewer {...diff} />;
-	case "D":
-		return <DiffDeletedViewer {...diff} />;
-	case "E":
-		return <DiffEditViewer {...diff} />;
-	default:
-		 return null;
-	}
-};
-
-const DiffNewViewer = (diff: DiffNew<unknown>) => {
-	return <span>{JSON.stringify(diff.rhs)}</span>;
-};
-
-const DiffDeletedViewer = (diff: DiffDeleted<unknown>) => {
-	return <span>{JSON.stringify(diff.lhs)}</span>;
-};
-
-const DiffEditViewer = (diff: DiffEdit<unknown>) => {
-	return <span>{`${JSON.stringify(diff.lhs)} ➞ ${JSON.stringify(diff.rhs)}`}</span>;
-};
-
-const diffNmspc = nmspc("diff");
-
-const mapDiffClassName = (kind: Diff<unknown>["kind"]) => {
-	switch (kind) {
-	case "N":
-		return diffNmspc("new");
-	case "D":
-		return diffNmspc("delete");
-	case "E":
-		return diffNmspc("edit");
-	default:
-		 return "";
-	}
-};
-
-const DiffViewerRow = (diff: Diff<unknown>) => (
-	<tr className={mapDiffClassName(diff.kind)}>
-		<th><DiffPath {...diff} /></th>
-		<td>
-			<DiffKindMapper {...diff} />
-		</td>
-	</tr>
+const DiffViewerModal = ({onHide, ...props}: Pick<GenericModalProps, "onHide"> & DiffViewerProps) => (
+	<GenericModal onHide={onHide}>
+		<DiffViewer {...props} />
+	</GenericModal>
 );
 
-const DiffsViewer = ({diffs}: {diffs: Diff<unknown>[]}) => {
-	const {theme} = React.useContext(Context);
-	const {Table} = theme;
-	return (
-		<Table bordered condensed>
-			<tbody>
-				{diffs.map(d => <DiffViewerRow key={d.path?.join() + d.kind} {...d}/>)}
-			</tbody>
-		</Table>
-	);
-};
+type GenericModalProps = {
+	onHide: () => void;
+} & HasChildren
 
-const GenericModal = ({onHide, children}: {onHide: () => void} & HasChildren) => {
+const GenericModal = ({onHide, children}: GenericModalProps) => {
 	const {theme} = React.useContext(Context);
 	const {Modal} = theme;
 	return (
